105d6eedb8f7e33c07de4a90b1a955390329d50d
Singgih Praditya <singgihpraditya@gmail.com>
2021-01-04 12:10:37 +0700

    initial release

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var dateModule = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World today is '+dateModule.myDateTime());
+  res.end('Hello World is '+dateModule.myDateTime());
 }).listen(8080);
